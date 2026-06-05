a398e56cbfd7ea5cf40a8bac80a0c25cb8ae8585
sdfltdit <126612571+sdfltdit@users.noreply.github.com>
2026-06-06 02:21:25 +0600

    Delete fix-last-link.cjs

--- fix-last-link.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-// Fix the last broken link
-let content = fs.readFileSync('src/pages/insights/AQL-inspection-explained.astro', 'utf8');
-content = content.replace('href="/insights/quality-control-checklist/index"', 'href="/insights/quality-control-checklist"');
-fs.writeFileSync('src/pages/insights/AQL-inspection-explained.astro', content);
-
-console.log('Fixed the last broken link');
